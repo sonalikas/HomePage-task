6ce77623cef44173fbec42233c5e1a642c651d09
sonalikas <sonalika1715@gmail.com>
2021-09-30 13:57:55 +0530

    [Add] intial commit

---
+++ home.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>BureWala Task</title>
+	<link rel="stylesheet" type="text/css" href="styles/style.css" />
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"> -->
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<!-- font awesome -->
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+</head>
+<body>
+	
+<!-- nav bar start -->
+<div class="navbar navbar" id="navbar" style="margin-bottom: 0px !important;">
+	<div style="display: flex;align-items: center; justify-content: space-between; padding: 0 50px;">
+		<div class="navbar-default">
+			<a href="home.php" >
+				<img src="images/tasklogo.jpg" alt="logo" class="hidden-xs" style="padding-top: 0px;">
+				<img src="images/tasklogo.jpg" alt="logo" class="visible-xs">
+			</a>
+			</div>
+		<!-- navbar menu star -->
+		<div>
+			<div>
+				<ul class="nav navbar-nav navbar-right">
+					<li class="active"><a href="home.php">Home</a></li>
+					<li><a href="services.php">Services</a></li>
+					<li><a href="portfolio.php">Portfolio</a></li>
+					<li><a href="testimonials.php">Testimonials</a></li>
+					<li><a href="contactus.php">Contact Us</a></li>
+					
+				</ul>
+			</div>
+			
+		</div>
+		<!-- navbar menu end -->
+	</div>
+	
+</div>
+<!-- navbar end -->
+
+
+
+<!-- slider start -->
+<div  id="slider" style="padding-bottom:50px !important; margin-top: 0px;">
+		<div class="carousel slide" id="mycarousel" data-ride="carousel">
+			<ol class="carousel-indicators">
+				<li data-target="mycarousel" data-slide-to="0" class="active"></li>
+				<li data-target="mycarousel" data-slide-to="1"></li><li data-target="mycarousel" data-slide-to="2"></li><li data-target="mycarousel" data-slide-to="3"></li><li data-target="mycarousel" data-slide-to="4"></li>
+			</ol>
+			<!-- start -->
+			<div class="carousel-inner">
+				<div class="item active">
+					<img src="images/Banner1.jpg">
+				</div>
+				<div class="item ">
+					<img src="images/1.jpg" >
+				</div>
+				<div class="item ">
+					<img src="images/Banner1.jpg">
+				</div>
+				<div class="item ">
+					<img src="images/1.jpg">
+				</div>
+				<div class="item ">
+					<img src="images/Banner1.jpg">
+				</div>
+			</div>
+			<!-- end -->
+			<a href="#mycarousel" class="left carousel-control" data-slide="prev"> 
+				<span class="glyphicon glyphicon-chevron-left"></span>
+			<span class="sr-only">previous</span>
+
+			</a>
+			<a href="#mycarousel" class="right carousel-control" data-slide="next"> 
+				<span class="glyphicon glyphicon-chevron-right"></span>
+			<span class="sr-only">Next</span>
+
+			</a>
+		</div>
+	
+</div>
+<!-- slider end -->
+<!-- 3 box start -->
+<div id="advantage">
+	<div class="container">
+		<div class="same-height-row">
+			<div class="col-sm-4">
+				<div class="box same-height">
+					<h3><a href="#"> Design/Build</a></h3>
+					<p>Design–build is a method to deliver a project in which the design and construction services are contracted by a single entity known as the design–builder or design–build contractor.</p>
+				</div>
+			</div>
+			<div class="col-sm-4">
+				<div class="box same-height">
+					<h3><a href="#">Pre-construction Design</a></h3>
+					<p>The cost estimate was formed by cost estimators on the construction team who then also prepare the bid. Pre-construction emerged from construction cost estimating to include. </p>
+				</div>
+			</div>
+			<div class="col-sm-4">
+				<div class="box same-height">
+					<h3><a href="#">Construction Management</a></h3>
+					<p>Construction management is a professional service that provides a project's owner(s) with effective management of the project's schedule, cost, quality, safety, scope, and function.</p>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+</div>
+<!-- 3 box end -->
+<div id="hotbox">
+	<div class="box">
+		<div class="container">
+			<div class="col-md-12">
+				<h2>Latest News of This Week</h2>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- hotbox end -->
+
+<div id="content"class="container">
+	<div class="row">
+			<div class="col-md-3 col-sm-6 center responsive"><!-- testimonials 1 start -->
+					<div class="testimonials">
+							<img src="images/Round-Face.png" class="img-responsive">
+						</a>
+						<div class="text">
+							<h3><a href="details.php">wanchuk </a></h3>
+							<p class="price">“I just wanted to share a quick note and let you know that you guys do a really good job. I'm glad I decided to work with you.”</p>
+							<p class="buttons">
+						 		<a href="details.php" class="btn btn-primary"><i class="fa fa-shoping-cart"></i> View more</a>
+							</p>
+						</div>
+					</div>
+					
+				</div><!-- testimonials 1 end -->
+
+					<div class="col-md-3 col-sm-6 center responsive"><!-- testimonials 1 start -->
+					<div class="testimonials">
+							<img src="images/Round-Face.png" class="img-responsive">
+						</a>
+						<div class="text">
+							<h3><a href="details.php">henry </a></h3>
+							<p class="price">“I just wanted to share a quick note and let you know that you guys do a really good job. I'm glad I decided to work with you.” </p>
+							<p class="buttons">
+						 		<a href="details.php" class="btn btn-primary"><i class="fa fa-shoping-cart"></i> View more</a>
+							</p>
+						</div>
+					</div>
+					
+				</div><!-- testimonials 1 end -->
+
+					<div class="col-md-3 col-sm-6 center responsive"><!-- testimonials 1 start -->
+					<div class="testimonials">
+							<img src="images/Round-Face.png" class="img-responsive">
+						</a>
+						<div class="text">
+							<h3><a href="details.php">Sonam ray </a></h3>
+							<p class="price">“I just wanted to share a quick note and let you know that you guys do a really good job. I'm glad I decided to work with you.”</p>
+							<p class="buttons">
+						 		<a href="details.php" class="btn btn-primary"><i class="fa fa-shoping-cart"></i> View more</a>
+							</p>
+						</div>
+					</div>
+					
+				</div><!-- testimonials 1 end -->
+
+					<div class="col-md-3 col-sm-6 center responsive"><!-- testimonials 1 start -->
+					<div class="testimonials">
+							<img src="images/Round-Face.png" class="img-responsive">
+						</a>
+						<div class="text">
+							<h3><a href="details.php">kanishk </a></h3>
+							<p class="price">“I just wanted to share a quick note and let you know that you guys do a really good job. I'm glad I decided to work with you.”</p>
+							<p class="buttons">
+						 		<a href="details.php" class="btn btn-primary"><i class="fa fa-shoping-cart"></i> View more</a>
+							</p>
+						</div>
+					</div>
+					
+				</div><!-- testimonials 1 end -->
+
+					
+					
+	</div>
+</div>
+
+
+<!-- footer start -->
+<?php
+include("includes/footer.php")
+?>
+<!-- footer end -->
+
+
+
+
+
+
+ <!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script></body>
+</html>
---
+++ includes/footer.php
@@ -0,0 +1,108 @@
+<div id="footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3 col-sm-6">
+				<h4>Pages</h4>
+				<ul>
+					<li>
+						<a href="services.php">Services</a>
+					</li>
+					<li>
+						<a href="Portfolio.php">Portfolio</a>
+					</li>
+					<li>
+						<a href="Contact.php">Contact us</a>
+					</li>
+					<li>
+						<a href="Testimonials.php">Testimonials</a>
+					</li>
+				</ul>
+				<hr>
+				<h4>User Section</h4>
+				<ul>
+					<li>
+						<a href="login.php">Login</a>
+					</li>
+					<li>
+						<a href="registration.php">Register</a>
+					</li>
+				</ul>
+				<hr class="hidden-md hidden-lg hidden-sm">
+			</div>
+			<div class="col-md-3 col-sm-6">
+				<h4>Top Services</h4>
+				<ul>
+					<li>
+						<a href="#">Emergency Services.</a>
+					</li>
+					<li>
+						<a href="#">Design/Build.</a>
+					</li>
+					<li>
+						<a href="#">Building Sustainability.</a>
+					</li><li>
+						<a href="#">Re modelling.</a>
+					</li><li>
+						<a href="#">Construction Services.</a>
+					</li>
+				</ul>
+				<hr class="hidden-md hidden-lg">
+			</div>
+			
+			<div class="col-md-3 col-sm-6">
+				<h4>Where to find us</h4>
+				<p>
+					<STRONG>Burewala.com</STRONG> <br> South ex
+					<br> Delhi
+					<br> India
+					<br> burewala@gmail.com
+					<br> +91 9999797706 
+				</p>
+				<a href="contact.php">Contact us</a>
+				<hr class="hidden-md hidden-lg">
+			</div>
+
+			<div class="col-md-3 col-sm-6">
+				<h4>Get the news</h4>
+				<p class="text-muted">
+					Subscribe here for getting news for burewala.
+				</p>
+				<form action="" method="post">
+					<div class="input-group">
+						<input type="text" name="email" class="font-control" style="height: 46px;
+    width: 96%;">
+						<span class="input-group-btn"><input type="submit" name="" class="btn btn-default" style="height: 46px; value="subscribe"></span> 
+						
+					</div>
+				</form>
+				<hr>
+				<h4>Stay In Touch</h4>
+				<p class="social">
+					<a href="#"><i class="fab fa-facebook-f"></i></a>
+					<a href="#"><i class="fab fa-twitter"></i></a>
+					<a href="#"><i class="fab fa-instagram"></i></a>
+					<a href="#"><i class="fab fa-google-plus"></i></a>
+					<a href="#"><i class="fa fa-envelope"></i></a>
+				</p>
+			</div>
+		</div>
+	</div>
+	
+</div>
+
+
+<!-- copy_right -->
+<div id="copyright">
+	<div class="container">
+		<div class="col-md-6">
+			<p class="pull-left">
+				&copy; 2021 | Sonalika Shrivastav
+			</p>
+		</div>
+		<div class="col-md-6">
+			<p class="pull-right">
+				tamplet by: <a href="#">burewala.com</a>
+			</p>
+		</div>
+	</div>
+</div>
